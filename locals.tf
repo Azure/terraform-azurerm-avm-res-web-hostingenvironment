db704b91bb37d15959cb2821b9597ba5df50d958
Isabelle Bersano <ibersano@microsoft.com>
2024-05-28 11:34:50 -0400

    fix: remove unused local var

--- locals.tf
+++ locals.tf
@@ -1,5 +1,4 @@
 locals {
-  resource_group_location            = try(data.azurerm_resource_group.parent[0].location, null)
   role_definition_resource_substring = "/providers/Microsoft.Authorization/roleDefinitions"
 }
 
