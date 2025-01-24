0b1e5c716c831d56822345333a328212f7cca4d0
Isabelle Bersano <ibersano@microsoft.com>
2025-01-24 13:09:35 -0500

    fix: remove unused tf provider

--- terraform.tf
+++ terraform.tf
@@ -5,10 +5,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 4.0"
     }
-    modtm = {
-      source  = "azure/modtm"
-      version = "~> 0.3"
-    }
     random = {
       source  = "hashicorp/random"
       version = "~> 3.5"
